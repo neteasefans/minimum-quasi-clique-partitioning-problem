a69fa0d2344cc844c30df67cb211a53c3a516bbc
Qing Zhou <zhouqing@mail.neu.edu.cn>
2025-09-02 16:17:27 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,28 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-		System.out.println("args.length = " + args.length);
-		for (int i = 0; i < args.length; i++) {
-			System.out.println(i + ": " + args[i]);
-		}
+		 // 确保至少前两个参数存在
+        if (args == null || args.length < 2) {
+            System.out.println("providing the first two parameters at least！");
+            return;
+        }
+
+        // 构建完整参数数组
+        String[] fullArgs = new String[4];
+        fullArgs[0] = args[0]; // 第1个参数来自命令行
+        fullArgs[1] = args[1]; // 第2个参数来自命令行
+
+        // 后两个参数使用默认值，如果命令行提供就覆盖
+        fullArgs[2] = (args.length >= 3) ? args[2] : "0";   // 默认值 0
+        fullArgs[3] = (args.length >= 4) ? args[3] : "12";  // 默认值 12
+
 
       Improve_new max9Test = new Improve_new();
 //      args = new String[]{"p-hat500-1.mtx", "r1.txt","0","12"};
-      max9Test.main(args);
+      max9Test.main(fullArgs);
 
 //        System.out.println("test");
     }
-
 
 
 }
